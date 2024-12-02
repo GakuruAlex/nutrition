6eb258446440c44c7445422cb9a71af0c2012faa
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-02 15:52:30 +0300

    no_of_calories

---
+++ nutrition.py
@@ -0,0 +1,44 @@
+class Nutrition:
+    def __init__(self):
+        self.fruits = {
+            "Apple": 130,
+            "Avocado California": 50,
+            "Banana": 110,
+            "Cantaloupe": 50,
+            "Grapefruit": 60,
+            "Grapes": 90,
+            "Honeydew Melon": 50,
+            "Kiwifruit": 90,
+            "Lemon": 15,
+            "Lime": 20,
+            "Nectarine": 60,
+            "Orange": 80,
+            "Peach": 60,
+            "Pear": 100,
+            "Pineapple": 50,
+            "Plums": 70,
+            "Strwberries": 50,
+            "Sweet Cherries": 100,
+            "Tangerine": 50,
+            "Watermelon": 80
+        }
+    def no_of_calories(self, fruit: str)-> int:
+        """_Given a fruit return the amount of calories in a portion of the fruit_
+
+        Args:
+            fruit (str): _Fruit to get calories_
+
+        Returns:
+            int: _Number of calories_
+        Example:
+        >>> no_of_calories("Apple")
+            130
+        """
+        return self.fruits[fruit]
+    def main(self)->None:
+        fruit = input("Item: ").title()
+        calories = self.no_of_calories(fruit)
+        print(f"Calories: {calories}")
+if __name__ == "__main__":
+    nutrition = Nutrition()
+    nutrition.main()
