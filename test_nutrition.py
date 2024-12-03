234390438b568a9c3e84bfdf46505604f22e5f63
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-03 10:35:07 +0300

    test no_of_calories with uppercase input

--- test_nutrition.py
+++ test_nutrition.py
@@ -11,3 +11,16 @@ class TestNoOfCaloriesWithLowercase:
     nutrition = Nutrition()
     def test_no_of_calories_with_lowercase_fruit(self, fruit, no_calories):
         assert self.nutrition.no_of_calories(fruit) == no_calories
+
+
+
+@pytest.mark.parametrize("fruit, no_calories",[
+    ("APPLE", 130),
+    ("AVOCADO CALIFORNIA", 50),
+    ("BANANA", 110),
+    ("HONEYDEW MELON", 50)
+ ])
+class TestNoOfCaloriesWithUppercase:
+    nutrition = Nutrition()
+    def test_no_of_calories_with_uppercase_fruit(self, fruit, no_calories):
+        assert self.nutrition.no_of_calories(fruit) == no_calories
